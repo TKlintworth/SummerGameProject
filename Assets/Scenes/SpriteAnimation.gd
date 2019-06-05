ff3741567837ba825aca6f90fe57fca333ef661f
carsonkaylor1 <ckaylor@email.sc.edu>
2019-06-05 12:56:51 -0400

    jab system

--- Assets/Scenes/SpriteAnimation.gd
+++ Assets/Scenes/SpriteAnimation.gd
@@ -7,22 +7,31 @@ var right = false
 var up = false
 var down = false
 
+var spear_point_pos
+export var spear_attack_bool = false
+var spear_ready
+
 onready var player_node = get_parent().get_node("CanvasLayer/Control/NinePatchRect/TextureProgress")
 
 func _ready():
 	$AnimatedSprite.play("slave_jab_spear_inactive")
+	spear_point_pos = $Area2D.position.x
+	spear_ready = true
+	
 
 
 func _physics_process(delta):
-	if(Input.is_key_pressed(KEY_LEFT)):
+	if(Input.is_key_pressed(KEY_LEFT) && spear_ready == true):
 		$AnimatedSprite.set_flip_h(false)
+		$Area2D.position.x = spear_point_pos
 		vel.x = -300
 		left = true
 		right = false
 		down = false
 		up = false
-	elif(Input.is_key_pressed(KEY_RIGHT)):
+	elif(Input.is_key_pressed(KEY_RIGHT) && spear_ready == true):
 		$AnimatedSprite.set_flip_h(true)
+		$Area2D.position.x = spear_point_pos * (-1)
 		vel.x = 300
 		left = false
 		right = true
@@ -47,7 +56,15 @@ func _physics_process(delta):
 	vel = move_and_slide(vel)
 	
 	if(Input.is_key_pressed(KEY_T)):
-		$AnimatedSprite.play("slave_throw_spear_active")
+		spear_ready = false
+		$Area2D.show()
+		$AnimatedSprite.play("slave_jab_spear_active")
+		if $AnimatedSprite.flip_h == false:
+			$Area2D/AnimationPlayer.play("spear_attack")
+		if $AnimatedSprite.flip_h == true:
+			$Area2D/AnimationPlayer.play("spear_attack_right")
+		spear_attack_bool = true
+		
 	
 	if(Input.is_key_pressed(KEY_Y)):
 		take_damage()
@@ -61,3 +78,6 @@ func take_damage():
 
 func _on_AnimatedSprite_animation_finished():
 	$AnimatedSprite.play("slave_jab_spear_inactive")
+	spear_attack_bool = false
+	$Area2D.hide()
+	spear_ready = true
