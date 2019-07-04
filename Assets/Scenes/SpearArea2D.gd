58a3a91a8921ef3736230b7f09878849b3cd63f4
carsonkaylor1 <ckaylor@email.sc.edu>
2019-07-04 09:33:54 -0500

    spear pickup

--- Assets/Scenes/SpearArea2D.gd
+++ Assets/Scenes/SpearArea2D.gd
@@ -1,18 +1,28 @@
 extends Area2D
 
+export var spear_pickup = false
+var main_scene = load("res://Scenes/MainFightScene.gd").new()
+var player_status
 
 func _ready():
-	pass
+	spear_pickup = false
+	player_status = get_tree().get_root().get_node("Node2D/Player").status
 
 func _process(delta):
 	var overlapping_bodies = get_overlapping_bodies()
 
-	if not overlapping_bodies:
+	if not overlapping_bodies: # if no overlapping bodies, do not run any more code
 			return
 	for body in overlapping_bodies:
-		if not body.is_in_group("Player"):
+		if not body.is_in_group("Player"): # if the overlapping body is not the player, do not run any more code
 			return
 		#body.take_damage()
 		print("hit")
-		body.hide()
+		#player_status = get_tree().get_root().get_node("Node2D/Player").status
+		#player_status = 0
+		print(main_scene)
+		main_scene.destroy_spear()
+		print(player_status)
+		spear_pickup = true
+		#get_parent().hide()
 	set_physics_process(false)
