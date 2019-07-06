614538719f772514791b42732147fc23af6f9b4b
carsonkaylor1 <ckaylor@email.sc.edu>
2019-07-06 14:43:55 -0500

    add enemy attack animation

---
+++ Assets/Scenes/EnemyArea2D.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+func _ready():
+	pass
+
+func _process(delta):
+	var overlapping_bodies = get_overlapping_bodies()
+
+	if not overlapping_bodies: # if no overlapping bodies, do not run any more code
+			return
+	for body in overlapping_bodies:
+		if not body.is_in_group("Player"):
+			return
+		get_parent().attack = true # enemy will perform attack animation
+
+func _on_AnimatedSprite_animation_finished():
+	get_parent().attack = false # enemy will stop playing attack animation
