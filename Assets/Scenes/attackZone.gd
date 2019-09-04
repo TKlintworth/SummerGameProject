4844b677134f55e8e019125d91b513766d3775b0
TKlintworth <frostflag@gmail.com>
2019-09-04 18:20:59 -0400

    working on ai

---
+++ Assets/Scenes/attackZone.gd
@@ -0,0 +1,15 @@
+extends CollisionShape2D
+
+signal _on_Area2D_area_entered
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
