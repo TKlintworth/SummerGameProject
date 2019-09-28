9ec9b9b8925bf2e6d4aa44c04ee2590426bf9af3
carsonkaylor1 <ckaylor@email.sc.edu>
2019-09-27 22:09:30 -0500

    random spear

--- Assets/Scenes/MainFightScene.gd
+++ Assets/Scenes/MainFightScene.gd
@@ -5,49 +5,25 @@ var new_wave = false
 var start_next_wave = false
 export (PackedScene) var enemy_scene
 export (PackedScene) var heavy_enemy_scene
+export (PackedScene) var spear_pickup_scene
 var enemy
 var heavy_enemy
+var spear_pickup
 var scene_path_to_load
+var rng = RandomNumberGenerator.new()
 
 func _ready():
 	$CanvasLayer/Control/Wave_Enemy_Spawn_Timer.wait_time = 2
 	heavy_enemy = heavy_enemy_scene.instance()
-	add_child(heavy_enemy)
+	self.add_child(heavy_enemy)
 	heavy_enemy.position = Vector2(500, 500)
-	#$Fog/icon/AnimationPlayer.play("fog_in") # play fog shader at start of game
-	#set_process(false) # set process to false to make less expensive
-	#print(get_tree().get_nodes_in_group("Enemy1").size())
+	rng.randomize()
+	var random_number = rng.randf_range(0, 1000)
+	add_spear(random_number)
 	
-func destroy_spear():
-	remove_child($Spear)
-	
-#func destroy_enemy():
-#	remove_child($Enemy)
-#	enemy_count -= 1
-#	if enemy_count <= 0:
-#		new_wave_start()
-	
-#func new_wave_start():
-#	$Fog/icon/AnimationPlayer.play("new_wave") #Plays shader
-#	wave_num += 1
-	#new_wave = true
-#	get_enemy_number()
-#	set_process(true)
-#	$CanvasLayer/Control/Timer.wait_time = 2 #Set wait timer for wave text
-#	$CanvasLayer/Control/Timer.start()
-	
-
-#func play_wave_text(): # Wave transition
-	#new_wave = false
-#	start_next_wave = true
-#	$CanvasLayer/Control/NinePatchRect/Wave_Small.progress() # increases wave count and plays wave text
-#	$CanvasLayer/Control/Wave_Enemy_Spawn_Timer.wait_time = 5 #Set wait timer for enemy wave to spawn
-#	$CanvasLayer/Control/Wave_Enemy_Spawn_Timer.start()
-
 func play_win():
 	scene_path_to_load = "res://Scenes/WinScreen.tscn"
-	get_node("/root/GameStateManager").getScene(scene_path_to_load)
-	
+	get_node("/root/GameStateManager").getScene(scene_path_to_load)	
 	
 func get_enemy_number():
 	match wave_num:
@@ -58,6 +34,11 @@ func get_enemy_number():
 
 func play_battle_music():
 	$Music.play_battle_music()
+
+func add_spear(ran_num):
+	spear_pickup = spear_pickup_scene.instance()
+	self.add_child(spear_pickup)
+	spear_pickup.position = Vector2(ran_num,ran_num)
 
 func start_new_wave():
 	wave_num += 1
@@ -82,6 +63,5 @@ func _process(delta):
 	if get_tree().get_nodes_in_group("Enemy1").size() <= 0 && new_wave == false:
 		new_wave = true
 		start_next_wave = true
-		print("new wave")
 		$CanvasLayer/Control/Wave_Enemy_Spawn_Timer.start()
 	
--- Assets/Scenes/SpearPickup.gd
+++ Assets/Scenes/SpearPickup.gd
@@ -1,14 +1,5 @@
 extends Area2D
-
-export var spear_pickup = false
-#var main_scene = load("res://Scenes/MainFightScene.gd").new()
-var player_status
-var spear_gone = false # tells if spear has hit enemy or not
-
-func _ready():
-	spear_pickup = false
-	player_status = get_tree().get_root().get_node("MainRoot/Player").player_status
-
+	
 func _process(delta):
 	var overlapping_bodies = get_overlapping_bodies()
 
@@ -20,6 +11,4 @@ func _process(delta):
 		print("player entered area")
 		get_parent().get_tree().get_root().get_node("MainRoot/Player").set_player_status(0)
 		get_parent().get_tree().get_root().get_node("MainRoot/Player").set_thrown(false)
-		self.queue_free()
-
-	set_physics_process(false) 
+		self.queue_free() 
