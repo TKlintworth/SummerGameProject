f871a0a70068f8b06135c3b769e9b435ea67028c
carsonkaylor1 <ckaylor@email.sc.edu>
2019-11-03 18:17:04 -0600

    spear flash, fast jab

---
+++ Assets/Scenes/CanvasModulate.gd
@@ -0,0 +1,13 @@
+extends CanvasModulate
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#self.color = "f8fa0a"
+	pass # Replace with function body.
+
+func blink_on():
+	self.color = "f8fa0a"
