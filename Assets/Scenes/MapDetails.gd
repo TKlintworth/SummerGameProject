bf7039c5de75b376f7a0407a3d96adbb393dc926
TKlintworth <frostflag@gmail.com>
2019-12-09 20:15:00 -0500

    added some menu noise closes #141, closes #128

---
+++ Assets/Scenes/MapDetails.gd
@@ -0,0 +1,11 @@
+extends VBoxContainer
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+onready var tween = get_node("Tween")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
